6b6b86c76fa1a8a1650af55c4eb34ff055d9c4f0
olga <bagira_to@mail.ru>
2022-11-26 18:11:11 +0300

    add failing

---
+++ failing/saving_data.py
@@ -0,0 +1,9 @@
+
+
+schoolfailing = "School_Failing.txt" #для оценок
+myfile = open(schoolfailing, "a+")
+myfile.close
+
+studentfailing = "Student_Failing.txt" #для списка учеников
+stfile = open(studentfailing, "a+")
+stfile.close
